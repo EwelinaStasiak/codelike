37e897036a86b480862f743ee355715a0b73f951
Jacek_R <jacek.ragann@gmail.com>
2017-08-30 22:09:21 +0200

    Create fight module with functions for dealing damage, and checking if attack hit. Stub of function for ending game.

---
+++ fight.py
@@ -0,0 +1,25 @@
+from random import randint
+
+
+def test_for_hit(to_hit, evade):
+    attacker_result = 0
+    defender_result = 0
+    for i in range(to_hit):
+        attacker_result += randint(1, 4)
+    for i in range(evade):
+        defender_result += randint(1, 3)
+    return attacker_result >= defender_result
+
+
+def deal_damage(damage, defense):
+    total_damage = 0
+    for i in range(damage):
+        total_damage += randint(1, 2)
+    if total_damage - defense < 1:
+        return 1
+    else:
+        return total_damage
+
+
+def end_game():
+    print('You have died')
