bcd075a659e6af3fbb61f4a14edd6ccdb406ace8
Jacek_R <jacek.ragann@gmail.com>
2017-08-31 12:51:38 +0200

    Remove prints and instead append message to messages list

--- inventory.py
+++ inventory.py
@@ -1,28 +1,32 @@
 from items import Food, Pants, Armor, Weapon
 
 
-def print_inventory(inventory):
+def print_inventory(inventory, messages):
     if not inventory:
         return print('The inventory is empty.')
     for i in range(len(inventory)):
 
         if isinstance(inventory[i], Weapon):
-            print(str(i + 1), "Type: {}".format(inventory[i].item_type), "Weight: {}".format(inventory[i].weight),
-                  "Rarity: {}".format(inventory[i].rarity),
-                  "Damage: {}".format(inventory[i].damage), "Hit Rate: {}".format(inventory[i].hit_rate))
+            messages.append(str(i + 1), "Type: {}".format(inventory[i].item_type),
+                            "Weight: {}".format(inventory[i].weight),
+                            "Rarity: {}".format(inventory[i].rarity),
+                            "Damage: {}".format(inventory[i].damage), "Hit Rate: {}".format(inventory[i].hit_rate))
         elif isinstance(inventory[i], Armor):
-            print(str(i + 1), "Type {}".format(inventory[i].item_type), "Weight: {}".format(inventory[i].weight),
-                  "Rarity: {}".format(inventory[i].rarity),
-                  "Defense: {}".format(inventory[i].defense), "Hit Points: {}".format(inventory[i].hit_points))
+            messages.append(str(i + 1), "Type {}".format(inventory[i].item_type),
+                            "Weight: {}".format(inventory[i].weight),
+                            "Rarity: {}".format(inventory[i].rarity),
+                            "Defense: {}".format(inventory[i].defense),
+                            "Hit Points: {}".format(inventory[i].hit_points))
         elif isinstance(inventory[i], Pants):
-            print(str(i + 1), "Type: {}".format(inventory[i].item_type), "Weight: {}".format(inventory[i].weight),
-                  "Rarity: {}".format(inventory[i].rarity),
-                  "Defense: {}".format(inventory[i].defense))
-
+            messages.append(str(i + 1), "Type: {}".format(inventory[i].item_type),
+                            "Weight: {}".format(inventory[i].weight),
+                            "Rarity: {}".format(inventory[i].rarity),
+                            "Defense: {}".format(inventory[i].defense))
         elif isinstance(inventory[i], Food):
-            print(str(i + 1), "Type: {}".format(inventory[i].item_type), "Weight: {}".format(inventory[i].weight),
-                  "Rarity: {}".format(inventory[i].rarity),
-                  "Heal Amount: {}".format(inventory[i].heal_amount))
+            messages.append(str(i + 1), "Type: {}".format(inventory[i].item_type),
+                            "Weight: {}".format(inventory[i].weight),
+                            "Rarity: {}".format(inventory[i].rarity),
+                            "Heal Amount: {}".format(inventory[i].heal_amount))
 
 
 def add_item(item, inventory):
