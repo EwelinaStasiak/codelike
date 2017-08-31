baea5bab27bc35a48133964194c79b783ad814d2
Jacek_R <jacek.ragann@gmail.com>
2017-08-31 10:59:41 +0200

    Change referencing of Tile class to Cell class. Change name of hero type constant. Remove needless printing.

--- player.py
+++ player.py
@@ -3,8 +3,8 @@ from inventory import *
 
 
 class Player:
-   ZDZISŁAW = 'Zdzisław'
-   HENRYK = 'Henryk'
+    ZDZISLAW = 'Zdzisław'
+    HENRYK = 'Henryk'
 
     def __init__(self, x, y, type_hero):
         self.type_hero = type_hero
@@ -12,7 +12,7 @@ class Player:
         self.y = y
         self.inventory = []
 
-        if type_hero == Player.ZDZISŁAW:
+        if type_hero == Player.ZDZISLAW:
             self.health = 50
             self.damage = 5
             self.defense = 3
@@ -37,35 +37,32 @@ def getch():
 
 
 def action_of_player(game_map, player):
-    print('Press W(up), S(down), A(left), D(right)')
     player_finished_turn = False
     input_moving = getch().upper()
     if input_moving == 'W':
-        if game_map[player.x][player.y - 1].tile == Tile.EMPTY:
-            game_map[player.x][player.y].tile = Tile.EMPTY
+        if game_map[player.x][player.y - 1].tile == Cell.EMPTY:
+            game_map[player.x][player.y].tile = Cell.EMPTY
             player.y -= 1
             player_finished_turn = True
     elif input_moving == 'S':
-        if game_map[player.x][player.y + 1].tile == Tile.EMPTY:
-            game_map[player.x][player.y].tile = Tile.EMPTY
+        if game_map[player.x][player.y + 1].tile == Cell.EMPTY:
+            game_map[player.x][player.y].tile = Cell.EMPTY
             player.y += 1
             player_finished_turn = True
     elif input_moving == 'A':
-        if game_map[player.x - 1][player.y].tile == Tile.EMPTY:
-            game_map[player.x][player.y].tile = Tile.EMPTY
+        if game_map[player.x - 1][player.y].tile == Cell.EMPTY:
+            game_map[player.x][player.y].tile = Cell.EMPTY
             player.x -= 1
             player_finished_turn = True
     elif input_moving == 'D':
-        if game_map[player.x + 1][player.y].tile == Tile.EMPTY:
-            game_map[player.x][player.y].tile = Tile.EMPTY
+        if game_map[player.x + 1][player.y].tile == Cell.EMPTY:
+            game_map[player.x][player.y].tile = Cell.EMPTY
             player.x += 1
             player_finished_turn = True
     elif input_moving == 'P':
         player_finished_turn = True
     elif input_moving == 'I':
         print_inventory(player.inventory)
-        # while True:
-        #     use_item(inventory)
 
-    game_map[player.x][player.y].tile = Tile.PLAYER
+    game_map[player.x][player.y].tile = Cell.PLAYER
     return player_finished_turn
