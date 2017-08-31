063942b2f09685adad91945d85d4589e836436a9
Jacek_R <jacek.ragann@gmail.com>
2017-08-31 12:52:23 +0200

    Add getting input from the player. Change the main game loop to fix improper way of printing messages.

--- main.py
+++ main.py
@@ -3,25 +3,44 @@ import os
 from game_map import import_map_from_file as create_map
 from game_map import print_map
 from game_map import search_for_player
-from player import action_of_player, Player
+from player import action_of_player, Player, check_input
 from monster import create_monsters, move_monsters, monsters_attack
 from screens import open_file
 
 
+def getch():
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
 def main():
     start_screen()
-
     game_map = create_map('example_level.txt')
     monsters = create_monsters(game_map)
     player_location = search_for_player(game_map)
     player = Player(player_location[0], player_location[1], Player.ZDZISLAW)
     os.system('cls' if os.name == 'nt' else 'clear')
     print_map(game_map)
+    messages = []
     while True:
-        if action_of_player(game_map, player, monsters):
+        player_input = getch().upper()
+        if check_input(player_input, game_map, player, messages):
+            action_of_player(player_input, game_map, player, monsters, messages)
             move_monsters(game_map, monsters)
+            monsters_attack(game_map, monsters, player, messages)
+            os.system('cls' if os.name == 'nt' else 'clear')
             print_map(game_map)
-            monsters_attack(game_map, monsters, player)
+        if messages:
+            for message in messages:
+                print(message)
+            messages.clear()
 
 
 def play_screen():
